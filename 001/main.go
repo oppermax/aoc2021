1c3ec1a2178815c96cfe76269167b8a4aa14cc17
Max Opperman <55201171+oppermax@users.noreply.github.com>
2021-12-01 21:18:55 +0100

    setup

---
+++ 001/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/oppermax/aoc2021"
+
+func main()  {
+
+}
---
+++ pkg/utils/utils.go
@@ -0,0 +1,60 @@
+package utils
+
+import (
+	"bufio"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"strconv"
+)
+
+func readFile(file *os.File) []string {
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		num := scanner.Text()
+
+		lines = append(lines, num)
+	}
+	return lines
+}
+
+func RemoveDuplicates(elements []string) []string {
+	encountered := map[string]bool{}
+	out := []string{}
+
+	for v := range elements {
+		if encountered[elements[v]] == true {
+		} else {
+			encountered[elements[v]] = true
+			out = append(out, elements[v])
+		}
+	}
+	return out
+}
+
+func GetLines(filepath string) []string {
+	inputFile, err := os.Open("input")
+	if err != nil {
+		log.Infof("Could not read file: %v", err)
+	}
+	return readFile(inputFile)
+}
+
+func GetLinesInt(filepath string) []int {
+	inputFile, err := os.Open("input")
+	if err != nil {
+		log.Infof("Could not read file: %v", err)
+	}
+	scanner := bufio.NewScanner(inputFile)
+	var lines []int
+	for scanner.Scan() {
+		str := scanner.Text()
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			log.WithError(err)
+		}
+
+		lines = append(lines, num)
+	}
+	return lines
+}
