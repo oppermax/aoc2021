98775a406758b8b14fdc9b37afd6404e2aad9653
Max Opperman <55201171+oppermax@users.noreply.github.com>
2021-12-02 11:29:15 +0100

    get star 4

--- 002/main.go
+++ 002/main.go
@@ -16,18 +16,30 @@ type submarineGen1 struct {
 	loc *location
 }
 
+type submarineGen2 struct {
+	loc *location
+	aim int
+}
+
 func (l *location) getResult() int{
 	return l.x * l.y
 }
 
+func (l *location) getCurrentLocation() {
+	log.Infof("X %d / Y %d", l.x, l.y)
+}
+
 func main()  {
 	lines := utils.OpenFile("input.txt")
-	s := submarineGen1{&location{}}
+	s1 := submarineGen1{&location{}}
+	s2 := submarineGen2{&location{}, 0}
 	for _, ins := range lines {
 		dir, dis := findInstruction(ins)
-		s.move(dir, dis)
+		s1.move(dir, dis)
+		s2.move(dir, dis)
 	}
-	log.Infof("Solution 1: %d", s.loc.getResult())
+	log.Infof("Solution 1: %d", s1.loc.getResult())
+	log.Infof("Solution 2: %d", s2.loc.getResult())
 }
 
 func (s *submarineGen1) moveVer(instruction int) {
@@ -59,4 +71,23 @@ func (s *submarineGen1) move(dir string, dis int) {
 	case "backwards":
 		s.moveHor(-dis)
 	}
+}
+
+func (s *submarineGen2) move(dir string, dis int) {
+	switch dir {
+	case "up":
+		s.aim -= dis
+	case "down":
+		s.aim += dis
+	case "forward":
+		s.loc.x += dis
+		s.loc.y += dis * s.aim
+	}
+	log.Info(dir," ", dis)
+	s.loc.getCurrentLocation()
+	s.getAim()
+}
+
+func (s *submarineGen2) getAim() {
+	log.Infof("Aim %d", s.aim)
 }
